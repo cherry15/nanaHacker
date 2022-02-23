7e72e9080a70656cb7f376bd08d092bb115cdf36
cherry15 <cherylanncolier@gmail.com>
2022-02-23 17:24:32 +0000

    about

--- src/pages/about/about.tsx
+++ src/pages/about/about.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import './about.css'
+import me from './me.png'
 
 export const About = () => {
   return (
     <div className={`page about`}>
+      <img src={me} alt="Nana hacker" className='me' />
       <h1>About</h1>
     </div>
   )
